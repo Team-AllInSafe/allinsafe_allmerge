4459478ebf45077e2738ade11b8badff184a115e
Kyy <dbsdud4520@tukorea.ac.kr>
2025-08-13 08:47:17 +0900

    이전에 했던 ui 자잘 수정 반영

--- main/src/ui/java/de/blinkt/openvpn/ac4_screenlock/Ac4_01_screenlock_main.kt
+++ main/src/ui/java/de/blinkt/openvpn/ac4_screenlock/Ac4_01_screenlock_main.kt
@@ -66,6 +66,7 @@ class Ac4_01_screenlock_main : ComponentActivity() {
                 //기능 중지
                 // 1. sharedPreference에 앱 비활성화 저장
                 pref.edit().putBoolean("screenlock_onoff", false).apply()
+                isAppActive=false
                 // 2. ui 기능 중지 상태로 바꿈
                 setUiInactive(binding)
                 // 3. vpn 멈춤(진짜 기능
@@ -75,6 +76,7 @@ class Ac4_01_screenlock_main : ComponentActivity() {
                 //기능 실행
                 // 1. sharedPreference에 앱 활성화 저장
                 pref.edit().putBoolean("screenlock_onoff", true).apply()
+                isAppActive=true
                 // 2. ui 기능 작동 상태로 바꿈
                 setUiActive(binding)
                 // 3. vpn 시작(진짜 기능
